b16547728f8edc161d5caa1aa31d849183ac3f7e
Jim de Beer <jim@vigour.io>
2016-08-04 18:05:06 +0200

    update table

--- test/index.js
+++ test/index.js
@@ -4,14 +4,15 @@ const test = require('tape')
 
 test('set data', (t) => {
   const state = new State({ inject: require('../') })
-
   state.set({
     db: {
       id: process.env.AMAZON_ID,
-      secret: process.env.AMAZON_SECRET
+      secret: process.env.AMAZON_SECRET,
+      // need to add a remove api as well!
+      table: 'test-table'
     }
   })
-
+  console.log('____X____')
   console.log('yo yo yo')
   t.end()
 })
