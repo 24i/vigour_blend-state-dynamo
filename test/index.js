a6336374bc270d2f096672246e44e25fdd9fe433
Jim de Beer <jim@vigour.io>
2016-08-04 17:02:59 +0200

    inject, correct package

--- test/index.js
+++ test/index.js
@@ -4,8 +4,12 @@ const test = require('tape')
 
 test('set data', (t) => {
   const state = s({
-    inject: require('../')
+    inject: require('../'),
+    db: {
+      id: process.env.AMAZON_ID,
+      secret: process.env.AMAZON_SECRET
+    }
   })
-  console.log('yo yo yo')
+  console.log('yo yo yo', state)
   t.end()
 })
