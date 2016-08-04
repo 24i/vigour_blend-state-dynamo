2ce4384dcd0d601f801cd94395f06773ccab39bd
Jim de Beer <jim@vigour.io>
2016-08-04 17:08:30 +0200

    config

--- test/index.js
+++ test/index.js
@@ -1,15 +1,17 @@
 'use strict'
-const s = require('vigour-state/s')
+const State = require('vigour-state')
 const test = require('tape')
 
 test('set data', (t) => {
-  const state = s({
-    inject: require('../'),
+  const state = new State({ inject: require('../') })
+
+  state.set({
     db: {
       id: process.env.AMAZON_ID,
       secret: process.env.AMAZON_SECRET
     }
   })
-  console.log('yo yo yo', state)
+
+  console.log('yo yo yo')
   t.end()
 })
