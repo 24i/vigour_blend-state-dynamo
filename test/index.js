cea783b3dc5ce9d6db29af41fac28fa052287136
Jim de Beer <jim@vigour.io>
2016-08-04 16:52:01 +0200

    correct pckg

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
 'use strict'
-const state = require('vigour-state')
+const s = require('vigour-state/s')
 const test = require('tape')
 
 test('set data', (t) => {
+  const state = s({
+    inject: require('../')
+  })
   console.log('yo yo yo')
   t.end()
 })
-
