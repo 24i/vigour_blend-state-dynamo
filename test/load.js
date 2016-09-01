3cb07017c58979c01d84a302b7577b3a8dec4fbb
Jim de Beer <jim@vigour.io>
2016-09-01 23:25:52 +0200

    laod based on context

--- test/load.js
+++ test/load.js
@@ -6,37 +6,6 @@ const AMAZON_ID = process.env.AMAZON_ID
 const AMAZON_SECRET = process.env.AMAZON_SECRET
 const out = require('./util').out
 
-// inject issues -- fix in observable
-test('load context', (t) => {
-  const state = new State({ inject: require('../') })
-  // loads whole table
-  state.set({
-    db: {
-      id: AMAZON_ID,
-      secret: AMAZON_SECRET,
-      table: testTable,
-      out,
-      define: {
-        extend: {
-          in (parse, val) {
-            val.key = val.context + '.' + val.key
-            return parse(val)
-          }
-        }
-      }
-    },
-    on: {
-      error (err) {
-        console.log(err)
-      }
-    }
-  })
-  state.db.load(() => {
-    t.same(state.token.deeper.hello.val, true, 'returns correct data')
-    t.end()
-  })
-})
-
 test('load a whole table', (t) => {
   const state = new State({ inject: require('../') })
   // loads whole table
@@ -62,6 +31,36 @@ test('load a whole table', (t) => {
     }
   })
   state.db.load('*', () => {
+    t.same(state.token.deeper.hello.val, true, 'returns correct data')
+    t.end()
+  })
+})
+
+test('load context', (t) => {
+  const state = new State({ inject: require('../') })
+  // loads whole table
+  state.set({
+    db: {
+      id: AMAZON_ID,
+      secret: AMAZON_SECRET,
+      table: testTable,
+      out,
+      define: {
+        extend: {
+          in (parse, val) {
+            val.key = val.context + '.' + val.key
+            return parse(val)
+          }
+        }
+      }
+    },
+    on: {
+      error (err) {
+        console.log(err)
+      }
+    }
+  })
+  state.db.load('token', () => {
     t.same(state.token.deeper.hello.val, true, 'returns correct data')
     t.end()
   })
